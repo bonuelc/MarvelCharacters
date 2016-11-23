dee3ae7761ecc6a6f86d9c4aab09956d2fe9a59c
bonuelc <bonuelc@gmail.com>
2016-11-23 13:51:46 -0700

    Implement CharacterCollectionViewCell
    This CollectionViewCell contains one UIImageView for the character's
    thumbnail.

---
+++ MarvelComicCharacters/CharacterCollectionViewCell.h
@@ -0,0 +1,15 @@
+//
+//  CharacterCollectionViewCell.h
+//  MarvelComicCharacters
+//
+//  Created by Christopher Bonuel on 11/23/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CharacterCollectionViewCell : UICollectionViewCell
+
+@property (nonnull) IBOutlet UIImageView *photoView;
+
+@end
---
+++ MarvelComicCharacters/CharacterCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CharacterCollectionViewCell.m
+//  MarvelComicCharacters
+//
+//  Created by Christopher Bonuel on 11/23/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+#import "CharacterCollectionViewCell.h"
+
+@implementation CharacterCollectionViewCell
+
+@end
