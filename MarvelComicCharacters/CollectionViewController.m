c13b2b3ac0c8173b9a77e5e4408e65b1221378ee
bonuelc <bonuelc@gmail.com>
2016-11-26 09:35:22 -0700

    Implement - (NSString *) generateMD5(_:)
    This will be used to generate a hash for authentication.
    
    # Conflicts:
    #	MarvelComicCharacters/CollectionViewController.m

--- MarvelComicCharacters/CollectionViewController.m
+++ MarvelComicCharacters/CollectionViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "CollectionViewController.h"
+#import <CommonCrypto/CommonDigest.h>
 
 @interface CollectionViewController ()
 
@@ -18,6 +19,20 @@ static NSString * const reuseIdentifier = @"characterCell";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+}
+
+- (NSString *) generateMD5:(NSString *) input
+{
+    const char *cStr = [input UTF8String];
+    unsigned char digest[16];
+    CC_MD5( cStr, strlen(cStr), digest );
+    
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    
+    for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
+        [output appendFormat:@"%02x", digest[i]];
+    
+    return  output;
 }
 
 #pragma mark <UICollectionViewDataSource>
