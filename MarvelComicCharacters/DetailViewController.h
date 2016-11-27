f9ee20dc4bb20ae14b3aeb4238b77f3bddf73ade
bonuelc <bonuelc@gmail.com>
2016-11-26 18:44:36 -0700

    Add a blank DetailViewController

---
+++ MarvelComicCharacters/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  MarvelComicCharacters
+//
+//  Created by Christopher Bonuel on 11/26/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ MarvelComicCharacters/DetailViewController.m
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.m
+//  MarvelComicCharacters
+//
+//  Created by Christopher Bonuel on 11/26/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+@end
