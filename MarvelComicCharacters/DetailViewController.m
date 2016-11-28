53f610efc567a04567751f2555ec8c5c16fcb441
bonuelc <bonuelc@gmail.com>
2016-11-28 15:13:21 -0700

    Dismiss DetailVC on tap

--- MarvelComicCharacters/DetailViewController.m
+++ MarvelComicCharacters/DetailViewController.m
@@ -26,10 +26,25 @@
 }
 
 - (void)setupGestures {
+    UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(handleTap:)];
+    [self.view addGestureRecognizer:tapGestureRecognizer];
+    
     UILongPressGestureRecognizer *longPressGestureRecognizer = [[UILongPressGestureRecognizer alloc]initWithTarget:self action:@selector(handleLongPress:)];
     longPressGestureRecognizer.minimumPressDuration = 0.5;
     [longPressGestureRecognizer setDelegate:self];
     [self.imageView addGestureRecognizer:longPressGestureRecognizer];
+}
+
+- (void)handleTap:(UITapGestureRecognizer*)sender {
+    
+    [UIView animateWithDuration:0.5 animations:^{
+        self.imageView.transform = CGAffineTransformMakeTranslation(0, -self.view.frame.size.height);
+        self.nameLabel.transform = CGAffineTransformMakeTranslation(-self.view.frame.size.width, 0);
+        self.descriptionLabel.transform = CGAffineTransformMakeTranslation(self.view.frame.size.width, 0);
+        self.view.alpha = 0;
+    } completion:^(BOOL finished) {
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }];
 }
 
 - (void)handleLongPress:(UILongPressGestureRecognizer*)sender {
